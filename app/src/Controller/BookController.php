0763331796963c28066be4f8bf7bb768c68c1041
Loan Besson <loan.besson@hotmail.fr>
2020-11-20 16:46:09 +0100

    Book : Index / Add / Edit / Show

---
+++ app/src/Controller/BookController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Form\BookType;
+use App\Repository\BookRepository;
+use Doctrine\Inflector\Rules\NorwegianBokmal\Inflectible;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BookController
+ * @package App\Controller
+ *
+ * @Route("/book", name="book_")
+ */
+class BookController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(BookRepository $bookRepository)
+    {
+        return $this->render('book/index.html.twig', [
+            'books' => $bookRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show(Book $book)
+    {
+        return $this->render('book/show.html.twig', [
+            'book' => $book
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new", methods={"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            return $this->redirectToRoute('book_show', ['id' => $book->getId()]);
+        }
+
+        return $this->render('book/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Book $book)
+    {
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('book_show', ['id' => $book->getId()]);
+        }
+
+        return $this->render('book/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ app/src/Entity/Test.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TestRepository::class)
+ */
+class Test
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetimetz")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isFun;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     * @return Test
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsFun()
+    {
+        return $this->isFun;
+    }
+
+    /**
+     * @param mixed $isFun
+     * @return Test
+     */
+    public function setIsFun($isFun)
+    {
+        $this->isFun = $isFun;
+        return $this;
+    }
+
+}
